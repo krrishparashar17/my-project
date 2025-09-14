69a5cf19e4a006cfd2189c510de37ac261d11c8f
krrish parashar <krrishparashar1@gmail.com>
2025-09-14 21:26:48 +0530

    Updating src

--- react-lab/Experiment-8/src/App.jsx
+++ react-lab/Experiment-8/src/App.jsx
@@ -1,30 +1,13 @@
-import React from "react";
-import ProductCard from "./Components/ProductCard";
-import "./App.css";
+import React from 'react'
+import './App.css'
+import LibraryManagement from './components/LibraryManagement'
 
 function App() {
-  const products = [
-    { name: "Laptop", price: "$800", stock: "In Stock" },
-    { name: "Smartphone", price: "$500", stock: "Out of Stock" },
-    { name: "Headphones", price: "$120", stock: "In Stock" },
-    { name: "Tablet", price: "$300", stock: "In Stock" }
-  ];
-
   return (
-    <div className="app-container">
-      <h1 className="heading">Product List</h1>
-      <div className="product-list">
-        {products.map((item, index) => (
-          <ProductCard
-            key={index}
-            name={item.name}
-            price={item.price}
-            stock={item.stock}
-          />
-        ))}
-      </div>
+    <div className="App">
+      <LibraryManagement />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
