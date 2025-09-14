f5e75e99f50f00a562d6ce63616022de33413f47
krrish parashar <krrishparashar1@gmail.com>
2025-09-14 22:04:53 +0530

    Update

--- react-lab/Experiment-9/src/App.jsx
+++ react-lab/Experiment-9/src/App.jsx
@@ -1,28 +1,10 @@
-import React from "react";
-import ProductCard from "./Components/ProductCard";
-import "./App.css";
+import ClassHierarchyDemo from './Components/ClassHierarchyDemo.jsx';
+import './App.css';
 
 function App() {
-  const products = [
-    { name: "Laptop", price: "$800", stock: "In Stock" },
-    { name: "Smartphone", price: "$500", stock: "Out of Stock" },
-    { name: "Headphones", price: "$120", stock: "In Stock" },
-    { name: "Tablet", price: "$300", stock: "In Stock" }
-  ];
-
   return (
-    <div className="app-container">
-      <h1 className="heading">Product List</h1>
-      <div className="product-list">
-        {products.map((item, index) => (
-          <ProductCard
-            key={index}
-            name={item.name}
-            price={item.price}
-            stock={item.stock}
-          />
-        ))}
-      </div>
+    <div className="container">
+      <ClassHierarchyDemo />
     </div>
   );
 }
